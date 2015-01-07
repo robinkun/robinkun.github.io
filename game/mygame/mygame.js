801e56ab14dba7b0fd6c080b5da744013406d64e
robinkun <robinkun96@gmail.com>
2015-01-07 23:49:08 +0900

    update

--- game/mygame/mygame.js
+++ game/mygame/mygame.js
@@ -1,10 +1,11 @@
 var W_WIDTH = 500;
 var W_HEIGHT = 500;
-var C_WIDTH = 20;
-var C_HEIGHT = 20;
+var C_WIDTH = 50;
+var C_HEIGHT = 50;
 var CELL_W = W_WIDTH / C_WIDTH;
 var CELL_H = W_HEIGHT / C_HEIGHT;
-var DEL_NUM = 3;
+var DEL_NUM = 4;
+var COLOR_NUM = 5;
 var canvas;
 var context;
 
@@ -18,8 +19,8 @@ window.onload = function() {
   canvas.height = W_HEIGHT;
 
   var scaleRate = Math.min(window.innerWidth/W_WIDTH, window.innerHeight/W_HEIGHT); // Canvas引き伸ばし率の取得
-  canvas.style.width = W_WIDTH*scaleRate+'px';
-  canvas.style.height = W_HEIGHT*scaleRate+'px';  // キャンバスを引き伸ばし
+  canvas.style.width = W_WIDTH*scaleRate*0.8+'px';
+  canvas.style.height = W_HEIGHT*scaleRate*0.8+'px';  // キャンバスを引き伸ばし
   context = canvas.getContext('2d');                // コンテキスト
 
   makePuyo();
@@ -41,22 +42,26 @@ function makeArray() {
 function update() {
   draw();
   if(!dropBlock()) {
-    //deletePuyo();
     makePuyo();
+    deletePuyo();
   }
 
-  setTimeout(update, 100);
+  setTimeout(update, 30);
 }
 
 function deletePuyo() {
   for(var i = 0; i < C_HEIGHT; i++) {
     for(var j = 0; j < C_WIDTH; j++) {
-      if(cell[j][i] != 0) {
+      if(cell[i][j] != 0) {
         var num = countPuyoJoin(j, i);
-        /*
         if(num >= DEL_NUM) {
-          deletePuyoJoin(j, i, cell[j][i]);
-        }*/
+          deletePuyoJoin(j, i, cell[i][j]);
+          /*
+          while(deletePuyoJoin(j, i, cell[i][j])) {
+            while(dropBlock());
+          }
+          */
+        }
       }
     }
   }
@@ -75,16 +80,20 @@ function deletePuyoJoin(x, y, col) {
   deletePuyoJoin(x-1, y, col);
   deletePuyoJoin(x, y-1, col);
   deletePuyoJoin(x, y+1, col);
+
+  return 1;
 }
 
 function countPuyoJoin(x, y) {
   var c= cell[y][x]; // ぷよの色
   var n = 1;     // 1で初期化しているのは自分もカウントするため。
   cell[y][x] = -1; // この場所をチェックした証として一時的に空白に
-  if (y-1>=0 && cell[y-1][x]==c) n += countPuyoJoin(x, y-1);  
-  if (y+1<=C_HEIGHT-1 && cell[y+1][x]==c) n += countPuyoJoin(x, y+1);
+  
+  if (y-1>=0 && cell[y-1][x]==c) n += countPuyoJoin(x, y-1);
+  if (y+1<C_HEIGHT && cell[y+1][x]==c) n += countPuyoJoin(x, y+1);
   if (x-1>=0 && cell[y][x-1]==c) n += countPuyoJoin(x-1, y);
-  if (x+1<=C_WIDTH-1 && cell[y][x+1]==c) n += countPuyoJoin(x+1, y);
+  if (x+1<C_WIDTH && cell[y][x+1]==c) n += countPuyoJoin(x+1, y);
+
   cell[y][x] = c;   // 色を戻す
   return n;
 }
@@ -92,7 +101,7 @@ function countPuyoJoin(x, y) {
 function makePuyo() {
   for(var i = 0; i < C_WIDTH; i++) {
     if(cell[0][i] == 0) {
-      cell[0][i] = Math.floor(Math.random() * 4);
+      cell[0][i] = Math.floor(Math.random() * COLOR_NUM);
     }
   }
 }
@@ -123,7 +132,7 @@ function drawCell() {
     for(var j = 0; j < C_WIDTH; j++) {
       switch(cell[i][j]) {
       case 0:
-        context.fillStyle = 'rgb(0, 0, 0)';          // 色update();
+        context.fillStyle = 'rgb(255, 255, 255)';          // 色update();
         break;
       case 1:
         context.fillStyle = 'rgb(255, 100, 100)';          // 色update();
@@ -133,6 +142,9 @@ function drawCell() {
         break;
       case 3:
         context.fillStyle = 'rgb(100, 100, 255)';          // 色update();
+        break;
+      case 4:
+        context.fillStyle = 'rgb(255, 255, 100)';          // 色update();
         break;
       }
       context.fillRect(j*CELL_W, i*CELL_H, j*CELL_W+CELL_W, i*CELL_H + CELL_H);
